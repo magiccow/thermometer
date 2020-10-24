346fab7b4a3dcff3297a534b1ee02cd7b227e4f3
Martyn Davies <martyn@weirdcrater.co.uk>
2020-10-24 23:36:07 +0100

    Arduino sketch for thermometer

---
+++ thermometer.ino
@@ -0,0 +1,67 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+ 
+// Data wire is plugged into pin 2 on the Arduino
+#define ONE_WIRE_BUS 2
+ 
+// Setup a oneWire instance to communicate with any OneWire devices 
+// (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+ 
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+ 
+void setup(void)
+{
+  // start serial port
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  
+  // Start up the library
+  sensors.begin();
+}
+ 
+
+void drawtemp(float temp) {
+  display.clearDisplay();
+  display.setCursor(0,32);             // Start at top-left corner
+  display.setTextSize(3);  
+  display.setTextColor(SSD1306_WHITE, SSD1306_BLACK); 
+  display.print(temp);
+  display.print(" C");
+  display.setTextSize(2);
+  display.setCursor(85,27);             // position degree symbol
+  display.print(" o");  
+  display.display();
+}
+ 
+void loop(void)
+{
+  // call sensors.requestTemperatures() to issue a global temperature
+  // request to all devices on the bus
+  Serial.print(" Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("DONE");
+
+  float f = sensors.getTempCByIndex(0);
+  drawtemp( f );
+  
+  delay(1000);
+}
